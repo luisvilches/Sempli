c53ea66b78c68761fffbf2e2915ae5e2fc7139a7
luisvilches <luisvilches@debian.luisvilches>
2017-05-28 01:03:23 -0400

    clean

--- index.js
+++ index.js
@@ -8,11 +8,3 @@ const routesPrivate = require('./routes/private');
 sempli.config();
 sempli.public('/',routes);
 sempli.private('/dashboard',routesPrivate);
-
-
-// config cloudinary
-cloudinary.config({ 
-  cloud_name: 'dowhile', 
-  api_key: '379999323567133', 
-  api_secret: 'SJ_2PkWm2ISda-R2gDE5KdJR3J4' 
-});
