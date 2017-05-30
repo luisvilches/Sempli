841b028d310b790398a60734173631e2aba51f80
luisvilches <luisvilches@debian.luisvilches>
2017-05-30 13:59:13 -0400

    clean

--- controllers/main.js
+++
@@ -1,5 +0,0 @@
-const sempli = require('../sempli')
-
-exports.index = (req,res,next) => {
-	res.json({message: 'Hello Sempli!!'});
-}
--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-//modulos
-const sempli = require('./sempli');
-const routes = require('./routes/public');
-const routesPrivate = require('./routes/private');
-
-sempli.config();
-sempli.public('/',routes);
-sempli.private('/dashboard',routesPrivate);
