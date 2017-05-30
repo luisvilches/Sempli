4f4957d2c6611e9c503a09fecc419412494c1d05
LuisVilches <lvilches21@gmail.com>
2017-05-30 14:29:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         port: process.env.PORT || 5000
     },
     db: {
-        connection: "mongodb://blog:blog@ds151461.mlab.com:51461/blog"
+        connection: "mongodb://"
     },
     TOKEN_SECRET: process.env.TOKEN_SECRET || "79d27Mh1swkXnqGp2GdB0Xl4zmTKox2T"
 }
